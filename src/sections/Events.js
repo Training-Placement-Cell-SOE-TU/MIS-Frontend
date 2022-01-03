9f48f448baa62bb4643c8975dde4df0b3e4fa9a9
Ranjit Kalita <ranjitkalita499@gmail.com>
2022-01-03 12:02:01 +0530

    cards

--- src/sections/Events.js
+++ src/sections/Events.js
@@ -1,22 +1,37 @@
 import React from 'react'
-import {Card } from "react-bootstrap"
+import Eventcard from '../components/Eventcard'
+import eventimg1 from '../components/images/eventimg1.png'
+
 const Events = () => {
     return (
         <section className="eventmain">
             <div className="event1">
             <span className="event1text">Events</span>
             </div>
-        
-            <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+        <Eventcard
+        title="Event's Name"
+        image={eventimg1}
+        text="IEEE Tezpur University, as part of IEEE day celebration brings for 
+        you an one day bootcamp on full stack development. Along with video tutorials,
+        work step by step on real world project and learn concepts from basics. Learn
+        best practices of software development from industry experts and clear all 
+        your doubts in an one to one interaction. Collaborate on an IEEE project and
+        get certification from IEEE. Mentor for the bootcamp will be Nityananda 
+        Gohain who is currently working as Software Engineer 1 in Atlan."
+        />
+            
+
+        <Eventcard
+        title="Event's Name"
+        image={eventimg1}
+        text="IEEE Tezpur University, as part of IEEE day celebration brings for 
+        you an one day bootcamp on full stack development. Along with video tutorials,
+        work step by step on real world project and learn concepts from basics. Learn
+        best practices of software development from industry experts and clear all 
+        your doubts in an one to one interaction. Collaborate on an IEEE project and
+        get certification from IEEE. Mentor for the bootcamp will be Nityananda 
+        Gohain who is currently working as Software Engineer 1 in Atlan."
+        />
         </section>
     )
 }
