4c478922db68cffc50912f9294a955060fff4bea
Ranjit Kalita <ranjitkalita499@gmail.com>
2022-01-07 02:43:59 +0530

    changes

--- src/components/Eventcard.js
+++ src/components/Eventcard.js
@@ -6,7 +6,7 @@ function Eventcard(props) {
         <Container className="econcard">
             <Card className='econcard1 border-0'>
                 <Card.Body className="eventbody">
-                    <Card.Title className="etext1"> {props.title} </Card.Title>
+                    <Card.Text className="etext1"> {props.title} </Card.Text>
                     <Card.Text className="etext3">{props.date}  </Card.Text>
                     <Card.Img variant="top" src={props.image} />
                     <Card.Text className="etext2">{props.text}  </Card.Text>
