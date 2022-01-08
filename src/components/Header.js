b88b088365027f39b6c7abd0f314bf2b876a18c3
Ranjit Kalita <ranjitkalita499@gmail.com>
2022-01-08 19:46:49 +0530

    smallscreens

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,47 +23,102 @@ const Header = () => {
           
                                                 }
 
-                                .btn-l {
-                                            padding: 8px 14px;
+                                                .btn-flat1 {
+                                                  background-color:#0066ff;
+                                                  color: white;
+                                                  border-radius:6px;
+                                                  font-size: 12px;
+                                                  
+            
+                                                  }
+      
+                                                  .btn-flat1:hover {
+                                                      background-color:#0066ff;
+                                                      color: white;
+                                                      border-radius:6px;
+                                                      font-size: 12px;
+                
+                                                      }
+
+                                .btn-ol {
+                                            padding: 4px 10px;
                                             
                                             }
                                 `}
                         </style>
-            <Navbar className= "navbaar"  variant="dark" expand="lg" collapseOnSelect>
-            
-                <div className="tulogo"> <a href="/"><img src={logo} alt="tulogo" width="60" height="60" /></a></div>
-                <Navbar.Brand  href="/" ><span className="logo" >Training and Placement Cell</span> <br/> <span className="logo2" >School of Engineering,Tezpur University</span> </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className='mx-auto naav'>
-                  <Nav.Link href="/events" ><span className="navlnk">Events</span></Nav.Link>
-                  <Nav.Link href="/placements" ><span className="navlnk">Placements</span></Nav.Link>
-                  <Nav.Link href="/jobs" ><span className="navlnk">Latest Job Postings</span></Nav.Link>
-                  <Nav.Link href="/news" ><span className="navlnk">News &amp; Notifications </span></Nav.Link>
-                  <Nav.Link href="/about" ><span className="navlnk">About Us </span></Nav.Link>
-                  <Dropdown>
-                    <Dropdown.Toggle variant='flat' size="l">
-                        More
-                    </Dropdown.Toggle>
 
-                      <Dropdown.Menu>
+                      {/* -------------BIG SCREENS----------- */}
+
+                        <span className='nav1con'>
+                        <Navbar className= "navbaar"  variant="dark" >
+            
+                        <div className="tulogo"> <a href="/"><img src={logo} alt="tulogo" width="60" height="60" /></a></div>
+                        <Navbar.Brand  href="/" ><span className="logo" >Training and Placement Cell</span> <br/> <span className="logo2" >School of Engineering, Tezpur University</span> </Navbar.Brand>
+                        
+                        <Nav className='mx-auto naav'>
+                        <Nav.Link href="/events" ><span className="navlnk">Events</span></Nav.Link>
+                        <Nav.Link href="/placements" ><span className="navlnk">Placements</span></Nav.Link>
+                        <Nav.Link href="/jobs" ><span className="navlnk">Latest Job Postings</span></Nav.Link>
+                        <Nav.Link href="/news" ><span className="navlnk">News &amp; Notifications </span></Nav.Link>
+                        <Nav.Link href="/about" ><span className="navlnk">About Us </span></Nav.Link>
+                        <Dropdown>
+                        <Dropdown.Toggle variant='flat' size="l">
+                          More
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
                         <Dropdown.Item href="http://www.tezu.ernet.in/" target="_blank"> TU Home</Dropdown.Item>
                         <Dropdown.Item href="/stories">Stories</Dropdown.Item>
                         <Dropdown.Item href="/visitors">Visitors</Dropdown.Item>
                         <Dropdown.Item href="/people">People</Dropdown.Item>
                         <Dropdown.Item href="/admin">Admin Console</Dropdown.Item>
+                        </Dropdown.Menu>
+                        </Dropdown>
+                        </Nav> 
+                        
+                        </Navbar>
 
-                      </Dropdown.Menu>
-                  </Dropdown>
-
-
-
-                </Nav> 
-  
-              </Navbar.Collapse>
+                        </span>
             
-            </Navbar>
 
+                      {/* ----------SMALL SCREENS------------ */}
+                      <span className="nav2con">
+                      <Navbar className= "navbaar2"  variant="dark" >
+                      <span className='sidetoside'>
+
+                      
+                      <div className="tulogo"> <a href="/"><img src={logo} alt="tulogo" width="60" height="60" /></a></div>
+                      <Navbar.Brand  href="/" ><span className="logo" >T&amp;P Cell</span> <br/> <span className="logo2" >SoE, Tezpur University</span> </Navbar.Brand>
+                      <Nav className='mx-auto naav'>
+                      <Dropdown>
+                        <span className="hb" >
+                        <Dropdown.Toggle variant='flat1' size="ol" >
+                      <div className="hbmenu"></div>
+                      <div className="hbmenu"></div>
+                      <div className="hbmenu"></div>
+                      </Dropdown.Toggle>
+                        </span>
+                      
+
+                      <Dropdown.Menu align="end">
+                      <Dropdown.Item href="/events" >Events</Dropdown.Item>
+                      <Dropdown.Item href="/placements" >Placements</Dropdown.Item>
+                      <Dropdown.Item href="/jobs" >Latest Job Postings</Dropdown.Item>
+                      <Dropdown.Item href="/news" >News &amp; Notifications </Dropdown.Item>
+                      <Dropdown.Item href="/about" >About Us </Dropdown.Item>
+                      <Dropdown.Item href="http://www.tezu.ernet.in/" target="_blank"> TU Home</Dropdown.Item>
+                      <Dropdown.Item href="/stories">Stories</Dropdown.Item>
+                      <Dropdown.Item href="/visitors">Visitors</Dropdown.Item>
+                      <Dropdown.Item href="/people">People</Dropdown.Item>
+                      <Dropdown.Item href="/admin">Admin Console</Dropdown.Item>
+                      </Dropdown.Menu>
+                      </Dropdown>
+                      </Nav> 
+                      </span>
+                      </Navbar>
+
+                      </span>
+           
             
           </header>
   )
