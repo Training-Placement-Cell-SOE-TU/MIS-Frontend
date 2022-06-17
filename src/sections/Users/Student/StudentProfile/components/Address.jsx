d60a3432913299d55e3bc8286f9af0b1fcdfc1fa
NoisyBotDude <ashishsarmah11@gmail.com>
2022-06-17 08:58:08 +0530

    feat: option to send address data

--- src/sections/Users/Student/StudentProfile/components/Address.jsx
+++ src/sections/Users/Student/StudentProfile/components/Address.jsx
@@ -1,7 +1,8 @@
 import './Education.scss'
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Checkbox, IconButton, makeStyles } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
     editBtn: {
@@ -42,7 +43,6 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
         whiteSpace: 'nowrap',
         verticalAlign: 'baseline',
-        borderRadius: '.40rem',
     },
     badgeText: {
         display: 'flex',
@@ -60,21 +60,124 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-export default function AddressInfo() {
+
+const ipAddress = process.env.REACT_APP_IP;
+const port = process.env.REACT_APP_PORT;
+
+export default function AddressInfo(props) {
     const classes = useStyles();
 
+    const [currStudentId, setCurrStudentId] = useState("")
+
+
+    const [permanentAddress, setPermanentAddress] = useState(props.profile.permanent_address)
+    const [city, setCity] = useState(null)
+    const [state, setState] = useState(null)
+    const [country, setCountry] = useState(null)
+    const [pincode, setPincode] = useState(null)
+    const [district, setDistrict] = useState(null)
+    const [addressInfo, setAdressInfo] = useState(null)
+    const [addressInfo2, setAdressInfo2] = useState(null)
+    const [isPermanentEqualsPresent, setIsPermanentEqualsPresent] = useState(false)
+
+    console.log(permanentAddress)
+
+    const [openAddressInfoModal, setOpenAddressInfoModal] = useState(false)
+
+    var headers = {"headers" : { "Authorization": `Bearer ${localStorage.getItem("access-token")}`}}
+
+    const handleAddressInfoClose = () => {
+        setOpenAddressInfoModal(false)
+
+        setPermanentAddress(null)
+        setCity(null)
+        setState(null)
+        setCountry(null)
+        setPincode(null)
+        setDistrict(null)
+        setAdressInfo(null)
+        setAdressInfo2(null)
+    }
+
+    const handleUpdateAddressInfo = (e) => {
+        e.preventDefault()
+
+        const data = {
+            "student_id": currStudentId,
+            "permanent_address": {
+                "city": city,
+                "state": state,
+                "country": country,
+                "pincode": pincode,
+                "district": district,
+                "address_info": addressInfo,
+                "address_info2": addressInfo2
+            },
+            "is_permanent_equals_present": isPermanentEqualsPresent,
+            "present_address": {
+                "city": "",
+                "state": "",
+                "country": "",
+                "pincode": "",
+                "district": "",
+                "address_info": "",
+                "address_info2": ""
+            }
+        }
+
+        console.log(data)
+
+        axios.put(`http://${ipAddress}:${port}/student/update/address`,data, headers )
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        }).finally(() => {
+            setOpenAddressInfoModal(false)
+
+            setPermanentAddress(null)
+            setCity(null)
+            setState(null)
+            setCountry(null)
+            setPincode(null)
+            setDistrict(null)
+            setAdressInfo(null)
+            setAdressInfo2(null)
+        })
+
+    }
+
+    console.log(props.profile.permanent_address)
+
+    const handleOpenAddressUpdate = () => {
+        setOpenAddressInfoModal(true)
+
+        setCurrStudentId(props.profile.student_id)
+        setCity(props.profile.permanent_address.city)
+        setState(props.profile.permanent_address.state)
+        setCountry(props.profile.permanent_address.country)
+        setPincode(props.profile.permanent_address.pincode)
+        setDistrict(props.profile.permanent_address.district)
+        setAdressInfo(props.profile.permanent_address.address_info)
+        setAdressInfo2(props.profile.permanent_address.address_info2)
+        setIsPermanentEqualsPresent(props.profile.is_permanent_equals_present)
+
+    }
+
+    console.log(props.profile)
     return(
         <div className='col-lg-6 cred-box'>
             <div className={classes.detailsHeader}>
                 <div className={classes.credHeader}>Address Info</div>
-                <IconButton className={classes.iconBtn}>
+                <IconButton className={classes.iconBtn} onClick={handleOpenAddressUpdate}>
                     <EditIcon className={classes.editIcon}/>
                 </IconButton>
             </div>
             <div className={classes.detailsBox}>
                 <div className={classes.fieldBox}>
                     <p className={classes.address}>Permanent Address <span className={classes.addressText}>
-                        Near Central Jail, Jail Road, Joraht, Assam, Pin - 785004
+                        Near Central Jail, Jail Road, Joraht, {}, Pin - {permanentAddress.pincode}
                     </span></p>
 
                     <p>
